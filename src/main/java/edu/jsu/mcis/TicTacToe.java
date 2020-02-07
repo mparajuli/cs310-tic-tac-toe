ef45d8b099a147ba2ec775458671e62287a2b71d
mparajuli <mparajuli@stu.jsu.edu>
2020-02-06 19:44:39 -0600

    Completed Part Two

--- src/main/java/edu/jsu/mcis/TicTacToe.java
+++ src/main/java/edu/jsu/mcis/TicTacToe.java
@@ -1,33 +1,40 @@
 package edu.jsu.mcis;
 
+import javax.swing.*;
+
 public class TicTacToe {
-    
+
     public static final int DEFAULT_WIDTH = 3;
-    
+
     public static void main(String[] args) {
-        
-        /* Set initial size of game board (default is 3x3) */
+       
+        /* Set initial size of game board to the default (which is 3 x 3) */
 
-        int width = DEFAULT_WIDTH;
+        int size = DEFAULT_WIDTH;
         
-        /* If a different size is provided as an argument, use it instead */
-
-        if(args.length >= 1) {
-            
+        /* If a different size is provided as a command-line argument, use 
+           this size instead of the default */
+        
+        if ( args.length > 0 ) {
             try {
-                width = Integer.parseInt(args[0]);
+                size = Integer.parseInt( args[0] );
+            } catch ( NumberFormatException e ) {
+                System.exit(1);
             }
-            catch(NumberFormatException e) {}
-            
         }
-        
+
         /* Create Controller */
 
-        TicTacToeController controller = new TicTacToeController(width);
+        TicTacToeController controller = new TicTacToeController( size );
         
-        /* Start Main Loop */
+        /* Start GUI */
+
+        JFrame win = new JFrame( "Tic-Tac-Toe" );
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        controller.start();
+        win.add( controller.getView() );
+        win.pack();
+        win.setVisible( true );
         
     }
     
--- src/main/java/edu/jsu/mcis/TicTacToeController.java
+++ src/main/java/edu/jsu/mcis/TicTacToeController.java
@@ -1,49 +1,81 @@
 package edu.jsu.mcis;
 
-import edu.jsu.mcis.TicTacToeModel.Result;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import static java.lang.Integer.parseInt;
+import javax.swing.JButton;
 
-public class TicTacToeController {
+public class TicTacToeController implements ActionListener {
 
     private final TicTacToeModel model;
     private final TicTacToeView view;
     
     /* CONSTRUCTOR */
 
-    public TicTacToeController(int width) {
+    public TicTacToeController( int width ) {
         
-        /* Initialize model, view, and width */
+        /* Initialize model and view */
 
-        model = new TicTacToeModel(width);
-        view = new TicTacToeView();
+        model = new TicTacToeModel( width );
+        view = new TicTacToeView( this, width );
         
     }
-
-    public void start() {
     
-        /* MAIN LOOP (repeats until game is over) */
-
-        /* Display the board using the View's "showBoard()", then use
-           "getNextMove()" to get the next move from the player.  Enter
-           the move (using the Model's "makeMark()", or display an error
-           using the View's "showInputError()" if the move is invalid. */
-
-        boolean gameOver;
-
-        do{
-            view.showBoard(model.toString());
-
-            TicTacToeMove move = view.getNextMove(model.isXTurn()); 
-            
-            if ( !model.makeMark(move.getRow(), move.getCol() ))
-                view.showInputError();
-            
-            gameOver = (model.getResult() != Result.NONE);
-        } while (!gameOver);
-
-        /* After the game is over, show the final board and the winner */
-        view.showBoard(model.toString());
-        view.showResult(model.getResult().toString());
+    public String getMarkAsString( int row, int col ) {
+        
+        return ( model.getMark( row, col ).toString() );
         
     }
+    
+    public TicTacToeView getView() {
+        
+        return view;
+        
+    }
+    
+    @Override
+    public void actionPerformed( ActionEvent event ) {
+        
+        /* This is the event handler for button clicks. */
+        
+        /* First, acquire a reference to the clicked button and get its name */
+        
+        String name = ( (JButton) event.getSource() ).getName();
+        
+        /* Parse the row and column from the name (see Line 30 of the View) */
 
+        int row = parseInt(name.substring(6, 7));
+        int col = parseInt(name.substring(7, 8));
+
+        /* Make a mark at the specified row and column */
+        
+        model.makeMark( row, col );
+       
+        /* Update the view to show the new mark */
+
+        view.updateSquares();
+        
+        /* Get new result */
+        
+        TicTacToeModel.Result result = model.getResult();
+        
+        /* If the game is over, disable the squares and show the result */
+
+        if ( result != TicTacToeModel.Result.NONE ) {
+
+            view.disableSquares();
+            view.showResult(result.toString());
+            
+        }
+        
+        /* Otherwise, leave the result field empty */
+        
+        else {
+            
+            view.clearResult();
+            
+        }
+
+    }
+    
 }
